6daef9c6b1aaa5e5c452e482e5f64b12e41975aa
Jeek386 <51415763+Jeek386@users.noreply.github.com>
2019-06-05 18:49:09 +0200

    Add files via upload

---
+++ register.php
@@ -0,0 +1,17 @@
+<?php
+
+// PARAMETRI DA MODIFICARE
+$WEBHOOK_URL = 'https://mrrobot.herokuapp.com/execute.php';
+$BOT_TOKEN = '700676735:AAG-r-piU7-idLYQBvJNYRy62aWfg8hqi88';
+
+// NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
+$API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
+$method = 'setWebhook';
+$parameters = array('url' => $WEBHOOK_URL);
+$url = $API_URL . $method. '?' . http_build_query($parameters);
+$handle = curl_init($url);
+curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+$result = curl_exec($handle);
+print_r($result);
