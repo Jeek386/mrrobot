77a6eb68375a44f8783e10cbaf8045538fea3cf2
Jeoshua <51415763+Jeek386@users.noreply.github.com>
2019-06-05 19:25:03 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -31,7 +31,45 @@ elseif($text=="domanda 2")
 }
 else
 {
-	$response = "Comando non valido!";
+	$response = "<?php
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
+if(!$update)
+{
+  exit;
+}
+$message = isset($update['message']) ? $update['message'] : "";
+$messageId = isset($message['message_id']) ? $message['message_id'] : "";
+$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
+$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
+$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
+$username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
+$date = isset($message['date']) ? $message['date'] : "";
+$text = isset($message['text']) ? $message['text'] : "";
+$text = trim($text);
+$text = strtolower($text);
+header("Content-Type: application/json");
+$response = '';
+if(strpos($text, "/start") === 0 || $text=="ciao")
+{
+	$response = "Ciao $firstname, benvenuto!";
+}
+elseif($text=="come mi chiamo?")
+{
+	$response = "Il tuo nome è $firstname";
+}
+elseif($text=="domanda 2")
+{
+	$response = "risposta 2";
+}
+else
+{
+	$response = "Giovanni non essere precipitoso è solo una beta...!";
+}
+$parameters = array('chat_id' => $chatId, "text" => $response);
+$parameters["method"] = "sendMessage";
+echo json_encode($parameters);
+";
 }
 $parameters = array('chat_id' => $chatId, "text" => $response);
 $parameters["method"] = "sendMessage";
