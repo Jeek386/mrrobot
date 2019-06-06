337bd8c1f72f282236b9d054f8be857e1b41b45b
Jeoshua <51415763+Jeek386@users.noreply.github.com>
2019-06-06 15:41:39 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -19,7 +19,7 @@ header("Content-Type: application/json");
 $response = '';
 if(strpos($text, "/start") === 0 || $text=="ciao") #test
 {
-	$response = "Ciao $firstname, benvenuto!";
+	$response = "Ciao $date, benvenuto!";
 }
 elseif($text=="come mi chiamo?")
 {
