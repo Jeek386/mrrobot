12de39c1220174959f3f73f85da5c9bd07fd7b31
Jeoshua <51415763+Jeek386@users.noreply.github.com>
2019-06-06 15:31:50 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -20,8 +20,6 @@ $response = '';
 if(strpos($text, "/start") === 0 || $text=="ciao") #test
 {
 	$response = "Ciao $firstname, benvenuto!";
-	or
-	$response = "Ciao $firstname, bentornato!";
 }
 elseif($text=="come mi chiamo?")
 {
